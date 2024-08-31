c47e1c1947479476f8edc3ae53d206745eaa4ce8
Seth Beaty <seth.a.beaty@gmail.com>
2024-08-31 14:47:33 -0400

    Upload event capture

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -92,8 +93,11 @@ function SucessSVG() {
 export function SimpleUploadButton() {
   const router = useRouter();
 
+  const posthog = usePostHog();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload-begin");
       toast(
         <div className="flex items-center gap-2">
           <LoadingSpinnerSVG />
