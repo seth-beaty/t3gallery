50e10058e01c8626fc21a2ce980f558c8fb4e865
Seth Beaty <seth.a.beaty@gmail.com>
2024-09-10 13:22:38 -0400

    Implemented ratelimiting, cleaned up svgs for toasts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,39 +1,37 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
- 
+import { ratelimit } from "~/server/ratelimit";
+
 const f = createUploadthing();
- 
-// FileRouter for your app, can contain multiple FileRoutes
+
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "8MB", maxFileCount: 40} })
-    // Set permissions and file types for this FileRoute
+  imageUploader: f({ image: { maxFileSize: "8MB", maxFileCount: 40 } })
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user =  auth();
+      const user = auth();
       console.log("user", user);
- 
-      // If you throw, the user will not be able to upload
+
       if (!user.userId) throw new UploadThingError("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+
+      const { success } = await ratelimit.limit(user.userId);
+
+      if (!success) throw new UploadThingError("Rate limit exceeded");
+
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
- 
+
       await db.insert(images).values({
         name: file.name,
         url: file.url,
         userId: metadata.userId,
       });
- 
+
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
