c00ca33274eaca62e0eb47c95d52bcc9fd32f0bb
Seth Beaty <seth.a.beaty@gmail.com>
2024-09-10 13:36:25 -0400

    Restrict which users can upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
 import { ratelimit } from "~/server/ratelimit";
@@ -11,9 +11,13 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "8MB", maxFileCount: 40 } })
     .middleware(async ({ req }) => {
       const user = auth();
-      console.log("user", user);
 
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient().users.getUser(user.userId);
+
+      if (fullUserData?.privateMetadata?.["can-upload"] !== "true")
+        throw new UploadThingError("User does not have permission to upload");
 
       const { success } = await ratelimit.limit(user.userId);
 
